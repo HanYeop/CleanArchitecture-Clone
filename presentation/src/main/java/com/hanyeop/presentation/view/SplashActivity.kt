7b32d9d5b1633da88bb4abae56cacb79081a4c76
super7615 <super7615@naver.com>
2022-04-19 10:57:58 +0900

    feat : view 영역 생성

---
+++ presentation/src/main/java/com/hanyeop/presentation/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.hanyeop.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hanyeop.presentation.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
