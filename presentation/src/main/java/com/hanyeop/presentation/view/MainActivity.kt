5a1dd22150b5d8395b5e6eaf761b778fe92d4412
super7615 <super7615@naver.com>
2022-04-22 17:32:47 +0900

    feat : MainActivity di

--- presentation/src/main/java/com/hanyeop/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.hanyeop.presentation.view
 
+import androidx.activity.viewModels
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.databinding.ActivityMainBinding
+import com.hanyeop.presentation.viewmodel.MainViewModel
 import com.pss.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun init() {
 
     }
