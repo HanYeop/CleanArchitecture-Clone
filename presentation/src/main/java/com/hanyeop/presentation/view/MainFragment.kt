2de778f3a42558189aa119e25d33643a927a005a
super7615 <super7615@naver.com>
2022-04-28 20:19:39 +0900

    feat : firestore 점수 불러오기

--- presentation/src/main/java/com/hanyeop/presentation/view/MainFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/MainFragment.kt
@@ -4,8 +4,10 @@ import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.hanyeop.presentation.R
+import com.hanyeop.presentation.adapter.ScoreRecyclerViewAdapter
 import com.hanyeop.presentation.databinding.FragmentMainBinding
 import com.hanyeop.presentation.viewmodel.MainViewModel
+import com.hanyeop.presentation.widget.extension.showVertical
 import com.pss.library.CountNumberEvent
 import com.pss.presentation.base.BaseFragment
 
@@ -15,6 +17,14 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     override fun init() {
         binding.fragment = this
         mainViewModel.getStatisticsDisplay()
+        mainViewModel.getScore()
+        if (mainViewModel.getStatisticsDisplayEvent.value != null)
+            CountNumberEvent.countNumber(
+                0,
+                mainViewModel.getStatisticsDisplayEvent.value!!,
+                binding.number,
+                1000
+            )
         observeViewModel()
     }
 
@@ -27,5 +37,14 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
             binding.number.text = ""
             CountNumberEvent.countNumber(0, it, binding.number, 1000)
         }
+
+        mainViewModel.getScoreEvent.observe(this){
+            initRecyclerView()
+        }
+    }
+
+    private fun initRecyclerView(){
+        binding.scoreRecyclerView.adapter = ScoreRecyclerViewAdapter(mainViewModel)
+        binding.scoreRecyclerView.showVertical(requireContext())
     }
 }
